486701188670d1ec0974798051b4c12a3190e5ae
RianOshwaldSeloio <rianoshwald.selorio@lorma.edu>
2024-11-25 23:08:16 +0800

    first commit

---
+++ src/main/java/com/finalsproject/finalsproject/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package com.finalsproject.finalsproject.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.finalsproject.finalsproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.finalsproject.finalsproject.controllers to javafx.fxml, javafx.base;
+    exports com.finalsproject.finalsproject.controllers;
+
+    opens com.finalsproject.finalsproject to javafx.fxml;
+    exports com.finalsproject.finalsproject;
+}
